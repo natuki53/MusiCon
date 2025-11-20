e065244668c1c29d83f5e40118d9f349c67a9b7b
tokizoku <0830takatori2005@gmail.com>
2025-11-20 10:26:44 +0900

    じゅ

---
+++ src/main/java/model/Music.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.sql.Time;
+
+public class Music {//Music情報格納
+	private String title;//タイトル
+	private String genre;//ジャンル
+	private String artist;//アーティスト名
+	private String lyricist;//作詞家
+	private String composer;//作曲家
+	private int releaseYear;//発売年
+	private int like;//いいね数
+	private Time music_time;//再生時間
+	
+	// コンストラクタ : フィールド設定
+	public Music(String title,String genre,String artist,String lyricist,String composer,int releaseYear,int like,Time music_time) {
+		this.title = title;
+		this.genre = genre;
+		this.artist = artist;
+		this.lyricist = lyricist;
+		this.composer = composer;
+		this.releaseYear = releaseYear;
+		this.like = like;
+		this.music_time = music_time;
+	}
+	
+	public String getTitle() { return title; }
+	public String getGenre() { return genre;}
+	public String getArtist() { return artist; }
+	public String getLyricist() { return lyricist; }
+	public String getComposer() { return composer; }
+	public int getReleaseYear() { return releaseYear; }
+	public int getLike() { return like; }
+	public Time getMusicTime() { return music_time;}
+	
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+public class User {//ユーザー情報格納
+	private String user_name;//ユーザー名
+	private String user_pass;//パスワード
+	
+	// コンストラクタ : フィールド設定
+	public User(String user_name,String user_pass){
+		this.user_name = user_name;
+		this.user_pass = user_pass;
+	}
+	public String getUserName() { return user_name; }
+	public String getUserPass() { return user_pass; }
+}
