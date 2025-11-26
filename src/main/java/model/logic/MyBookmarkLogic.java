caaab3ee61c64f1c178623e3da9da03a7582d090
masaya123456 <abcd1234chiikawa@gmail.com>
2025-11-26 10:14:08 +0900

    ジュ

--- src/main/java/model/logic/MyBookmarkLogic.java
+++ src/main/java/model/logic/MyBookmarkLogic.java
@@ -2,11 +2,13 @@ package model.logic;
 
 import java.util.List;
 
+import model.Music;
+
 public class MyBookmarkLogic {
 	private BookmarkDAO dao = new BookmarkDAO();
 	
 	//マイページ情報の取得
-	public List<Bookmark> get( ) {
-		List<Bookmark> allList = dao.get();
+	public List<Music> get( ) {
+		List<Music> allList = dao.get();
 	}
 }
