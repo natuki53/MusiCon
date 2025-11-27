6e380e224cac72951a4ab086faba5612452025c7
masaya123456 <abcd1234chiikawa@gmail.com>
2025-11-27 09:57:13 +0900

    じゅ

--- src/main/java/model/logic/LoginUserLogic.java
+++ src/main/java/model/logic/LoginUserLogic.java
@@ -1,14 +1,21 @@
 package model.logic;
 
 import dao.UserDAO;
+import model.User;
 
 public class LoginUserLogic {
 
 	//ログイン機能
 	public boolean execute(User user) {
+		
+		// User オブジェクトからパスワードと名前を取得
+		String nm = user.getUserName();
+		String pw = user.getUserPass();
+		
 		//DAOインスタンス生成
 		UserDAO dao = new UserDAO();
-		if(dao./*メソッド名*/(user)) {
+		
+		if(dao.executeLogin(nm,pw)) {
 			//ログイン成功
 			return true;
 		}
