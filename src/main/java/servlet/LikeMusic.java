ea5aeceaf51592b0f825383ab09cfc72fcef0971
otosi <otosi@192.168.1.11>
2025-11-30 22:48:55 +0900

    ちゅかれたん

--- src/main/java/servlet/LikeMusic.java
+++ src/main/java/servlet/LikeMusic.java
@@ -3,12 +3,14 @@ package servlet;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import model.logic.PlayMusicLogic;
 
+@WebServlet("/LikeMusic")
 public class LikeMusic extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
